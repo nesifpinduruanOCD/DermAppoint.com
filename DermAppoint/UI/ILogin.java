1266fd64d482151e5e6c3b1ad132547c83863bb9
nesifpinduruanOCD <nesifpinduruan@gmail.com>
2026-01-12 17:42:36 +0800

    Delete DermAppoint directory

--- DermAppoint/UI/ILogin.java
+++
@@ -1,7 +0,0 @@
-package UI;
-
-public interface ILogin {
-    boolean login(String email, String password);
-    void logout();
-    boolean changePassword(String oldPassword, String newPassword);
-}
--- DermAppoint/UI/INotification.java
+++
@@ -1,8 +0,0 @@
-package UI;
-
-public interface INotification {
-    void sendAppointmentConfirmation(String recipient, String appointmentDetails);
-    void sendReminder(String recipient, String appointmentDetails);
-    void sendRescheduleNotice(String recipient, String oldDetails, String newDetails);
-    void sendCancellationNotice(String recipient, String appointmentDetails);
-}
